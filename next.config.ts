bec3f85584228ea2304f3ab65af33fdb4c8c5685
Arifin opu <41758862+ArifinS@users.noreply.github.com>
2025-09-27 21:30:26 +0600

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // Generate static "out" folder
+  images: {
+    unoptimized: true, // For <Image /> in static export
+  },
 };
 
 export default nextConfig;
